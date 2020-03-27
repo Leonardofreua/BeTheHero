0d70828bd31618d5b5b46c5a7e81535b1ed280e8
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-03-27 15:40:18 -0300

    Configure the Splash Screen.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#e02041" />
